318e0e204875d4f4957f8c9848c3237e3611e85a
문태주/Taeju <79253468+taeju-moon@users.noreply.github.com>
2025-08-09 12:01:21 +0900

    chore/remove-useless-type (#11)

--- types/complex.ts
+++ types/complex.ts
@@ -28,15 +28,3 @@ export interface DetailDocument extends DocumentType {
   additionalInfo: SectionRevisionType | null | undefined;
   reviews: ReviewType[];
 }
-
-export interface CreateDocumentDto {
-  created_by: string;
-  dong: string;
-  gu: string;
-  title: string;
-  location: string;
-  introduction: SectionRevisionType;
-  feature: SectionRevisionType;
-  additionalInfo: SectionRevisionType;
-  Hashtags: HashtagType[];
-}
